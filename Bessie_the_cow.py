83ed55528b8cab7b211a7f2106abf8b6da9e5222
LemonPhase <lemonphase.zzz@gmail.com>
2021-12-16 12:13:48 +0800

    Update Bessie_the_cow.py

--- Bessie_the_cow.py
+++ Bessie_the_cow.py
@@ -12,16 +12,15 @@ b1_x1, b1_y1, b1_x2, b1_y2 = int(lst[0].split()[0]), int(
     lst[0].split()[1]), int(lst[0].split()[2]), int(lst[0].split()[3])
 
 b2_x1, b2_y1, b2_x2, b2_y2 = int(lst[1].split()[0]), int(
-    lst[2].split()[1]), int(lst[1].split()[2]), int(lst[1].split()[3])
+    lst[1].split()[1]), int(lst[1].split()[2]), int(lst[1].split()[3])
 
 t_x1, t_y1, t_x2, t_y2 = int(lst[2].split()[0]), int(
     lst[2].split()[1]), int(lst[2].split()[2]), int(lst[2].split()[3])
 
-if b1_x1 <= t_x1 <= b1_x2:
-    # A part of truck in board 1
-    if b1_y1 <= t_y1 <= b1_y2:
-        a_1 = (b1_x2 - b1_x1) * (b1_y2 - b1_y2) - \
-            (b1_x2 - t_x1) * (b1_y2 - t_y1)
-    # A point of truck
-    else:
-        pass
+a1 = (b1_x2 - b1_x1) * (b1_y2 - b1_y1)
+a2 = (b2_x2 - b2_x1) * (b2_y2 - b2_y1)
+print(a1, a2)
+
+# Board 1
+a1 -= (b1_x2 - t_x1) * \
+    (t_y2 - t_y1) if b1_x1 < t_x1 < b1_x2 and b1_y1 <= t_y1 <= b1_y2 else 0
